53fc38db25d4606bdd2e367bf9d0a6e7c5dbe532
“CaesarBourne” <caesaradek@gmail.com>
2024-04-01 07:18:37 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Emmanuel!");
+string adjective = "resourceful";
+Console.WriteLine($"Caesra is smart and is hungry and {adjective} ");
+
+string work = "  Hello World   ";
+string newWork = work.TrimStart();
+Console.WriteLine(newWork.Contains("O"));
+int a = 18;
+int b = 6;
+int c = a + b;
+Console.WriteLine(c);
+
+int man = 2100000000;
+int woman = 2100000000;
+
+long solution = checked(man + woman);
+Console.WriteLine($"SOLUTION EMMANUEL {solution}");
